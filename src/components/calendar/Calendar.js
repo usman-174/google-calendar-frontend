9bc5f0dd507846c9e9a59ee82d14e691dcff24ec
usman-174 <hellmughal123@gmail.com>
2022-01-12 02:10:53 +0500

    fixed

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Calendar = () => {
   
     return (
-      <>
         <iframe
         src={`https://calendar.google.com/calendar/embed?wkst=1&
         title=IFrame_Calendar&bgcolor=%232c7a7b&ctz=Asia%2FKarachi&showTabs=1&showPrint=0&showCalendars=0&src=M2hjZWYxNnNrNnE3cG5uOGpsc2w4aGdwcnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F09300`}
@@ -18,8 +17,7 @@ const Calendar = () => {
         title='Calendar'
         scrolling="no"
       ></iframe>
-      
-      </>
+
     )
 }
 
