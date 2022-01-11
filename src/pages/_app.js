46554053a89b3698b3ea11460bef681e2550df67
usman-174 <hellmughal123@gmail.com>
2022-01-12 03:35:40 +0500

    x2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,27 @@
-import Head from 'next/head';
-import { CacheProvider } from '@emotion/react';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import { createEmotionCache } from '../utils/create-emotion-cache';
-import { theme } from '../theme';
-import  { SWRConfig } from 'swr'
+import Head from "next/head";
+import { CacheProvider } from "@emotion/react";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { createEmotionCache } from "../utils/create-emotion-cache";
+import { theme } from "../theme";
+import { SWRConfig } from "swr";
 import axios from "axios";
-import { ChakraProvider } from '@chakra-ui/react'
-import "../style.css"
-axios.defaults.baseURL=process.env.NEXT_APP_SERVER_URL || process.env.SERVER_URL||"https://iframe-calendar.herokuapp.com/"
+import { ChakraProvider } from "@chakra-ui/react";
+import "../style.css";
+axios.defaults.baseURL =
+  process.env.NEXT_APP_SERVER_URL ||
+  process.env.SERVER_URL ||
+  "https://iframe-calendar.herokuapp.com/";
 const fetcher = async (url) => {
   try {
-    const { data } = await axios.get(url)
-    return data
-      
+    const { data } = await axios.get(url);
+    return data;
   } catch (error) {
-      throw error.response.data
+    throw error.response.data;
   }
-}
+};
 const clientSideEmotionCache = createEmotionCache();
 
 const App = (props) => {
@@ -28,34 +30,27 @@ const App = (props) => {
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <CacheProvider value={emotionCache}>
-       <SWRConfig 
-      value={{
-        revalidateOnFocus:false,
-        refreshInterval: 600000,
-        shouldRetryOnError:false,
-        fetcher
-      }}>
-
-      <Head>
-        <title>
-          IFRAME CALENDAR APP
-        </title>
-        <meta
-          name="viewport"
-          content="initial-scale=1, width=device-width"
-        />
-      </Head>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <ChakraProvider>
-
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {getLayout(<Component {...pageProps} />)}
-        </ThemeProvider>
-        </ChakraProvider>
-      </LocalizationProvider>
+      <SWRConfig
+        value={{
+          revalidateOnFocus: false,
+          refreshInterval: 600000,
+          shouldRetryOnError: false,
+          fetcher,
+        }}
+      >
+        <Head>
+          <title>IFRAME CALENDAR APP</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <ChakraProvider>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              {getLayout(<Component {...pageProps} />)}
+            </ThemeProvider>
+          </ChakraProvider>
+        </LocalizationProvider>
       </SWRConfig>
-
     </CacheProvider>
   );
 };
